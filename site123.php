201c6b97b31480467366dedd11a39a465533f25d
Shedosexy <shedosexy@gmail.com>
2026-04-22 18:38:39 +0100

    Update site123.php

--- site123.php
+++ site123.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-/* ================= LIGAÇÃO 1111BASE DEiugriuge DADOS ================= */
+/* ================= LIGAÇÃO BASE DADOS ================= */
 $ligacao = new mysqli("localhost", "root", "", "biblioteca_db");
 if ($ligacao->connect_error) { die("Erro na ligação: " . $ligacao->connect_error); }
 
